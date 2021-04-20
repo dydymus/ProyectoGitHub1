d34eebd2ec3a355302a1f24fd902f549fc1cc9da
Usuario <Usuario@192.168.1.6>
2021-04-20 14:38:44 +0200

    Alejandro, he añadido un for con un if, para que te diga si el contador.

--- ProyectoGitHub1/src/proyectoGitHub1/ProyectoGitHub1.java
+++ ProyectoGitHub1/src/proyectoGitHub1/ProyectoGitHub1.java
@@ -5,7 +5,20 @@ public class ProyectoGitHub1
 
 	public static void main(String[] args) 
 	{
+		int contador=0;
 		System.out.println("Proyecto GITHUB 1");
+		
+		System.out.println("enviado por Nico");
+		//Nuevo añadido
+		for(int i =0; i <5; i++)
+		{
+			contador +=(i+1);
+			if(contador<5)
+			{
+				System.out.println("mi valor es menor de cinco");
+			}
+			else {System.out.println("mi valor es mayor o igual de cinco");}
+		}
 
 	}
 
